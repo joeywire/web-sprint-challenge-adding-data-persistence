46e4c1475713b5718cbbee86e00ee5436b870a78
joeywire <61367542+joeywire@users.noreply.github.com>
2020-12-11 10:13:18 -0600

    Migration complete and functional

--- data/migrations/20201211094306_initial_table_creation.js
+++ data/migrations/20201211094306_initial_table_creation.js
@@ -9,10 +9,47 @@ exports.up = function(knex) {
       table.boolean('completed')
         .notNullable()
         .defaultTo(0);
-    });
+    })
+    .createTable('tasks', table => {
+      table.increments('task_id'); 
+      table.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id').inTable('projects')
+        .onDelete('RESTRICT').onUpdate('RESTRICT'); 
+      table.string('description', 128)
+        .notNullable();
+      table.string('notes', 440); 
+      table.boolean('completed')
+        .notNullable()
+        .defaultTo(0);
+    })
+    .createTable('resources', table=> {
+      table.increments('resource_id'); 
+      table.string('name', 128)
+        .unique()
+        .notNullable(); 
+      table.string('description', 128); 
+    })
+    .createTable('project_resources', table => {
+      table.increments('id'); 
+      table.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id').inTable('projects')
+        .onDelete('RESTRICT').onUpdate('RESTRICT');
+      table.integer('resource_id')
+        .unsigned()
+        .notNullable()
+        .references('resource_id').inTable('resources')
+        .onDelete('RESTRICT').onUpdate('RESTRICT');
+    })
 };
 
 exports.down = function(knex) {
   return knex.schema
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('tasks')
     .dropTableIfExists('projects'); 
 };
