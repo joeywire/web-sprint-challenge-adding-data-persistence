01d8e8675852786c1db337b5bc7eb7ec3890ec10
joeywire <61367542+joeywire@users.noreply.github.com>
2020-12-11 16:00:42 -0600

    replaced poorly build middleware with more appropriate helper function

--- api/project/router.js
+++ api/project/router.js
@@ -1,12 +1,26 @@
 // build your `/api/projects` router here
 const express = require('express'); 
 const Project = require('./model');
-const middleware = require('../middleware'); 
+
 
 const router = express.Router();
 
-router.get('/', middleware.convertProjBool, async (req, res) => {
-  res.status(200).json(req.projects); 
+const convertCompletedBool = (array) => {
+  const workingArray = [...array]; 
+  workingArray.forEach(item => {
+    item.completed === 0 ? item.completed = false : item.completed = true;
+  }); 
+  return workingArray;
+}; 
+
+router.get('/', async (req, res) => {
+  try { 
+    const allProjects = await Project.getAll();
+    const cleanedProjects = convertCompletedBool(allProjects);
+    res.status(200).json(cleanedProjects); 
+  } catch (err) {
+    res.status(500).json({ message: err.message }); 
+  }
 });
 
 router.post('/', async (req, res) => {
