25f0be6f1de3e9197edb6ad698121e838958f5b2
joeywire <61367542+joeywire@users.noreply.github.com>
2020-12-11 09:29:53 -0600

    initial commit - env set up

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,21 @@
 module.exports = {
   development: {
-    // complete your knexfile
+    client: 'sqlite3', 
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/projects.db3'
+    },
     migrations: {
       directory: "./data/migrations",
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
     },
   },
 };
