6ceec5afb39f47bf28cbdc734ed6dbe81a0363b3
joeywire <61367542+joeywire@users.noreply.github.com>
2020-12-11 15:25:12 -0600

    Project middleware to convert boolean

--- api/project/router.js
+++ api/project/router.js
@@ -1,16 +1,12 @@
 // build your `/api/projects` router here
 const express = require('express'); 
 const Project = require('./model');
+const middleware = require('../middleware'); 
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  try {
-    const projects = await Project.getAll();
-    res.status(200).json(projects);
-  } catch (err) {
-    res.status(500).json({ message: err.message }); 
-  }
+router.get('/', middleware.convertProjBool, async (req, res) => {
+  res.status(200).json(req.projects); 
 });
 
 router.post('/', async (req, res) => {
