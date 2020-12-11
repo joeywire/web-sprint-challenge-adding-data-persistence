259e15eb280a7e516224e2a6571bde2af032bb9e
joeywire <61367542+joeywire@users.noreply.github.com>
2020-12-11 11:00:13 -0600

    Server up and running

--- api/server.js
+++ api/server.js
@@ -1,1 +1,19 @@
 // build your server here
+const express = require('express'); 
+const morgan = require('morgan'); 
+
+//import routes:
+
+
+//Create our server applicatoin
+const server = express();
+
+//Globl Middleware
+server.use(express.json()); 
+server.use(morgan('tiny')); 
+
+server.get('/', (_, res) => {
+  res.status(200).json({ API: "UP AND RUNNINGGGGG!" }); 
+})
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-// start your server here
+
 const server = require('./api/server'); 
 
+server.listen(5000, () => {
+  console.log(`\n-- Server Listening on port 5000 --\n`); 
+}); 
